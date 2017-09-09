15650a37cafb424d3febeb140447655e4848def9
Nafies Luthfi <nafiesl@gmail.com>
2017-09-09 23:00:37 +0800

    Add some attribute to lecturing endpoint

--- app/Responses/Tbkmk.php
+++ app/Responses/Tbkmk.php
@@ -24,14 +24,19 @@ class Tbkmk implements Responsable
             return [
                 'periode' => $value->THSMSTBKMK,
                 'prodi_id' => 7,
+                'lecturer_id' => null,
+                'lecturer_code' => $value->NODOSTBKMK,
+                'course_id' => null,
                 'course_code' => $value->KDKMKTBKMK,
                 'course_name' => $value->NAKMKTBKMK,
                 'sks' => $value->SKSMKTBKMK,
                 'sks_tm' => $value->SKSTMTBKMK,
                 'sks_prak' => $value->SKSPRTBKMK,
                 'sks_prak_lap' => $value->SKSLPTBKMK,
-                'semester' => $value->SEMESTBKMK,
-                'lecturer_code' => $value->NODOSTBKMK,
+                'sks_sim' => 0,
+                'semester' => (int) $value->SEMESTBKMK,
+                'capacity' => 50,
+                'class_code' => '01',
             ];
         });
     }
